af8f34825ba565024b2ea371f3f2ce4a23ff0482
AmanDeep9925 <amandeep58257@gmail.com>
2020-05-31 21:26:34 +0530

    Added Favourites Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,57 +5,116 @@
 
 // const api_url = "https://www.superheroapi.com/api.php/2656567927911258/search/";
 
-const searchBtn = document.querySelector("#search");
+    const searchBtn = document.querySelector("#search");
 
-const searchInput = document.getElementById('searchByname');
+    const searchInput = document.getElementById('searchByname');
+    
+    const herosContainer = document.querySelector(".hero-container");
+    
+    let heros = [];
+    
+    function getData(url) {
+        fetch(url)
+            .then(res => res.json())
+            .then((data) => {
+                heros = data.results;
+            })
+            .catch(err => console.log("error : " + err));
+    }
+    
+    searchBtn.addEventListener('click', () => {
+        const url = `https://www.superheroapi.com/api.php/2656567927911258/search/${searchInput.value}`;
+    
+        getData(url);
+    
+        heros.forEach(hero => {
+            herosContainer.innerHTML += `
+            <div class="hero-card" >
+            <div class="hero-image-container"">
+              <img
+                src="${hero.image.url}"
+                alt=""
+              />
+            </div>
+            <div class="hero-info-container">
+              <p>${hero.name}</p>
+            </div>
+            <div class="more-info" data-key=${hero.id}>
+              <a href="#" class="about">About</a>
+              <i class="far fa-star favourite" ></i>
+            </div>
+          </div>`   
+        });
+    })
 
-const herosContainer = document.querySelector(".hero-container");
 
-let heros = [];
-
-function getData(url) {
-    fetch(url)
-        .then(res => res.json())
-        .then((data) => {
-            heros = data.results;
+    // list of favourites superheros
+    const favourites = [];
+    
+    const addFav = document.querySelector('#heros');
+    
+    addFav.addEventListener("click",(event) =>{
+        if(event.target.classList.contains("favourite")){
+            key = event.target.parentElement.dataset.key;
+            addToFavourites(key);
+            event.target.classList.toggle('fas')
+        }
+    })
+    
+    function addToFavourites(key){
+        heros.forEach(hero =>{
+            if(hero.id === key){
+                favourites.push(hero);
+            }
         })
-        .catch(err => console.log("error : " + err));
-}
+    }
 
-searchBtn.addEventListener('click', () => {
-    const url = `https://www.superheroapi.com/api.php/2656567927911258/search/${searchInput.value}`;
+    const home = document.getElementById('HOME');
+    const favContainer = document.getElementById("favouriteHero");
 
-    getData(url);
+    const fav = document.getElementById('FAV');
+    const HContainer = document.getElementById("home")
 
-    heros.forEach(hero => {
-        herosContainer.innerHTML += `<div class="hero-card" >
-        <div class="hero-image-container">
-          <img
-            src="${hero.image.url}"
-            alt=""
-          />
-        </div>
-        <div class="hero-info-container">
-          <p>${hero.name}</p>
-        </div>
-        <div class="more-info">
-          <a href="#" class="about">About</a>
-          <i class="far fa-star favorite"></i>
-        </div>
-      </div>`   
-    });
+
+    home.addEventListener('click',()=>{
+        
+        // console.log("hi");
+        favContainer.style.display = "none";
+        HContainer.style.display ="block";
+    })
 
     
-},setTimeout(5000,() =>{
-    herosContainer.innerHTML = '';
-})
+
+    fav.addEventListener('click',()=>{
+        
+        // console.log("hi");
+        HContainer.style.display = "none";
+        favContainer.style.display = "block";
+        const favContainers = document.getElementById('fav-heros');
+        favourites.forEach(hero => {
+            favContainers.innerHTML +=`
+            <div class="hero-card" >
+            <div class="hero-image-container"">
+              <img
+                src="${hero.image.url}"
+                alt=""
+              />
+            </div>
+            <div class="hero-info-container">
+              <p>${hero.name}</p>
+            </div>
+            <div class="more-info" data-key=${hero.id}>
+              <a href="#" class="about">About</a>
+              <i class="fas fa-star favourite" ></i>
+            </div>
+          </div>
+            `
+        });
+    })
 
 
 
-
-
-
-
+// rendering the favourite superhero
 
 
 
