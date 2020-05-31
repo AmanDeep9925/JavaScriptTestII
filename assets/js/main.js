e26afbdc6d126d189068fc55bcd46f59726302ec
AmanDeep9925 <amandeep58257@gmail.com>
2020-05-31 19:47:05 +0530

    Search Handled

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,59 @@
 // Manage to add the hero in favorites
 
 
-const api_url = "https://www.superheroapi.com/api.php/2656567927911258/search/captainamerica";
+// const api_url = "https://www.superheroapi.com/api.php/2656567927911258/search/";
+
+const searchBtn = document.querySelector("#search");
+
+const searchInput = document.getElementById('searchByname');
+
+const herosContainer = document.querySelector(".hero-container");
+
+let heros = [];
 
 function getData(url) {
     fetch(url)
-    .then(res => res.json())
-    .then((data) => {
-        console.log(data.results[0])
-    })
+        .then(res => res.json())
+        .then((data) => {
+            heros = data.results;
+        })
+        .catch(err => console.log("error : " + err));
 }
-//call getData function
-getData(api_url);
+
+searchBtn.addEventListener('click', () => {
+    const url = `https://www.superheroapi.com/api.php/2656567927911258/search/${searchInput.value}`;
+
+    getData(url);
+
+    heros.forEach(hero => {
+        herosContainer.innerHTML += `<div class="hero-card" >
+        <div class="hero-image-container">
+          <img
+            src="${hero.image.url}"
+            alt=""
+          />
+        </div>
+        <div class="hero-info-container">
+          <p>${hero.name}</p>
+        </div>
+        <div class="more-info">
+          <a href="#" class="about">About</a>
+          <i class="far fa-star favorite"></i>
+        </div>
+      </div>`   
+    });
+
+    
+},setTimeout(5000,() =>{
+    herosContainer.innerHTML = '';
+})
+
+
+
+
+
+
+
+
+
+
